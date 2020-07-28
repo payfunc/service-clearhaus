d7ab1ecfbd8738c0b57d7008cd37a1d528be629a
Robin Nedergård <robin.nedergard@payfunc.com>
2020-07-28 16:02:04 +0200

    fix

--- api/Cardfunc.ts
+++ api/Cardfunc.ts
@@ -4,5 +4,5 @@ import { Configuration } from "./Configuration"
 
 export async function post<Request, Response>(configuration: Configuration, endpoint: string, request: Request): Promise<Response | gracely.Error> {
 	const response = await fetch(configuration.url + "/" + endpoint, { method: "POST", headers: { "Content-Type": "application/json; charset=utf-8", "authorization": "Bearer " + configuration.key }, body: JSON.stringify(request) }).catch(_ => undefined)
-	return !response ? gracely.server.unavailable() : response.ok ? response.json() : gracely.server.backendFailure(response.headers.get("Content-Type")?.startsWith("application/json") ? await response.json() : await response.text())
+	return !response ? gracely.server.unavailable() : response.headers.get("Content-Type")?.startsWith("application/json") ? response.json() : response.text()
 }
