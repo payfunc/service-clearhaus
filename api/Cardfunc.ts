1a9e4c062d3fb71bc40de9fce361dfcae72cb6ce
Robin Nedergård <robin.nedergard@payfunc.com>
2020-07-28 15:28:37 +0200

    cardfunc bearer authentication fix

--- api/Cardfunc.ts
+++ api/Cardfunc.ts
@@ -3,6 +3,6 @@ import * as gracely from "gracely"
 import { Configuration } from "./Configuration"
 
 export async function post<Request, Response>(configuration: Configuration, endpoint: string, request: Request): Promise<Response | gracely.Error> {
-	const response = await fetch(configuration.url + "/" + endpoint, { method: "POST", headers: { "Content-Type": "application/json; charset=utf-8", "authorization": configuration.key }, body: JSON.stringify(request) }).catch(_ => undefined)
+	const response = await fetch(configuration.url + "/" + endpoint, { method: "POST", headers: { "Content-Type": "application/json; charset=utf-8", "authorization": "Bearer " + configuration.key }, body: JSON.stringify(request) }).catch(_ => undefined)
 	return !response ? gracely.server.unavailable() : response.ok ? response.json() : gracely.server.backendFailure(response.headers.get("Content-Type")?.startsWith("application/json") ? await response.json() : await response.text())
 }
