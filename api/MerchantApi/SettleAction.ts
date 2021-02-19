39cdacb6ce484617b9af5216a33c6421ad942e8d
Robin Nedergård <robin.nedergard@payfunc.com>
2021-02-19 13:56:05 +0100

    date format

--- api/MerchantApi/SettleAction.ts
+++ api/MerchantApi/SettleAction.ts
@@ -1,12 +1,12 @@
-import { MerchantInfo } from "./MerchantInfo"
+import { Merchant } from "./Merchant"
 import { OrderAction } from "./OrderAction"
 
 export type SettleAction = {
-	merchant: MerchantInfo
+	merchant: Merchant
 	action: OrderAction
 }
 export namespace SettleAction {
 	export function is(value: any | SettleAction): value is SettleAction {
-		return typeof value == "object" && MerchantInfo.is(value.merchant) && OrderAction.is(value.action)
+		return typeof value == "object" && Merchant.is(value.merchant) && OrderAction.is(value.action)
 	}
 }
